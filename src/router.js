61218fbb4c46e010d83bbc78c18a27027f6b9ce1
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-04-24 20:12:42 +0700

    setup router

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import  Home  from "./components/Home.vue";
+import  Portfolio  from "./components/portfolio/Portfolio.vue";
+import  Stocks  from "./components/stocks/Stocks.vue";
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/portfolio", component: Portfolio },
+  { path: "/stocks", component: Stocks },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
