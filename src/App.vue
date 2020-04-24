ef0bef28a44e9f6e6398ae5d6c95b62427edf858
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-04-24 23:12:35 +0700

    add animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <div class="row">
       <div class="col-sm-12">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -27,4 +29,34 @@ export default {
 body {
   padding: 30px;
 }
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30);
+    opacity: 0;
+  }
+}
 </style>
+
